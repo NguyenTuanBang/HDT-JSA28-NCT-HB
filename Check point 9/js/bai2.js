acd72e7d844d9c6daa04da8e0fccf8ee9986c4e6
NguyenTuanBang <bang212006@gmail.com>
2023-07-29 15:30:28 +0700

    neww

--- Check point 9/js/bai2.js
+++ Check point 9/js/bai2.js
@@ -4,8 +4,10 @@
 const getPokemon = () => {
     const url = "https://pokeapi.co/api/v2/pokemon/1"
     fetch(url)
-      .then(res => res.json())
-      .then(data => {console.log(data);
-        pokemonName.innerHTML=data.sys.sunrise;
-  })
-  getPokemon()
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            pokemonName.innerHTML = data.sys.sunrise;
+        })
+}
+getPokemon()
